cb5c40e72d7f2c3614fd81831b12bf93b93200ae
Dean <fovhy100@gmail.com>
2017-01-16 23:13:41 -0600

    finished the jap-cn dict

---
+++ dict.py
@@ -0,0 +1,22 @@
+#!python3
+#This is a simple script to search strings in the clipboard to a JP-CN online dict, and 
+# 	it is best to use with something like python, might add ability search other langauge
+#   if I were to learn some new language in the future 
+import webbrowser
+import pyperclip
+import time
+
+timer = 0.5        # a timer to avoid blocking
+max_words_length = 20 # default max limit for length of a word
+old_string = pyperclip.paste()
+# the website(s) to search words on
+jap_website_name = "http://dict.hjenglish.com/jp/jc/"
+# check if it is the same or not 
+while(True):
+	new_string = pyperclip.paste()
+	if(new_string != old_string and len(new_string) < max_words_length):
+		old_string = new_string
+		webbrowser.open(jap_website_name + old_string)
+	time.sleep(timer)
+
+# if it is not the same search the phrase in the dictionary
