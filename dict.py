c505b5f3d97100c5467af7741c49f3a492c7b220
Dean <fovhy100@gmail.com>
2017-01-16 23:14:58 -0600

    slight fixes

--- dict.py
+++ dict.py
@@ -12,11 +12,10 @@ old_string = pyperclip.paste()
 # the website(s) to search words on
 jap_website_name = "http://dict.hjenglish.com/jp/jc/"
 # check if it is the same or not 
+# if it is not the same search the phrase in the dictionary
 while(True):
 	new_string = pyperclip.paste()
 	if(new_string != old_string and len(new_string) < max_words_length):
 		old_string = new_string
 		webbrowser.open(jap_website_name + old_string)
 	time.sleep(timer)
-
-# if it is not the same search the phrase in the dictionary
